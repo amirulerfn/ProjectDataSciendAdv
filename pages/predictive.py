767bc9e1c4ee20a2b82ed374cb9fe75f4a1ce085
Amirul Erfan <93848257+amirulerfn@users.noreply.github.com>
2025-01-11 02:15:10 +0800

    Update predictive.py

--- pages/predictive.py
+++ pages/predictive.py
@@ -1,5 +1,5 @@
 import pandas as pd
-df = pd.read_csv('/content/telur kelantan filtered.csv')
+df = pd.read_csv('telur kelantan filtered.csv')
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder, StandardScaler
