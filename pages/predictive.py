9c1231cd45884ce381c99e723bc1951ba3899b06
Amirul Erfan <93848257+amirulerfn@users.noreply.github.com>
2025-01-11 02:25:31 +0800

    Update predictive.py

--- pages/predictive.py
+++ pages/predictive.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import streamlit as st
+import matplotlib.pyplot as plt  # Make sure this is imported
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 from sklearn.ensemble import RandomForestRegressor
